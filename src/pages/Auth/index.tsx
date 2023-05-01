07729542b2b4beaf00bcb5249b780338d1a57b60
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-05-01 14:05:37 +0400

    adding temporary auth UI

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -3,28 +3,30 @@ import { Link } from "react-router-dom";
 const Auth: React.FC = () => {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-neutral-950">
-      <form className="flex flex-col justify-center items-center bg-neutral-900 px-6 py-4 gap-4 rounded-sm">
+      <form className="flex flex-col justify-center items-center bg-neutral-900 p-6 gap-4 rounded-sm">
         <div className="flex flex-col items-center gap-1">
-          <h1 className="text-indigo-200 text-2xl">Authorization</h1>
-          <span className="text-indigo-200 text-lg">
+          <h1 className="text-indigo-50 text-2xl">Authorization</h1>
+          <span className="text-indigo-50 text-lg">
             log in to jellyfin server
           </span>
         </div>
         <input
           type="text"
           name="username"
+          placeholder="Username"
           autoFocus
-          className="bg-neutral-800 p-2 rounded-md"
+          className="bg-neutral-800 p-2 rounded-md text-violet-50"
         />
         <input
-          type="text"
-          name="username"
+          type="password"
+          name="password"
+          placeholder="••••••••"
           autoFocus
-          className="bg-neutral-800 p-2 rounded-md"
+          className="bg-neutral-800 p-2 rounded-md text-violet-50"
         />
         <button
           type="submit"
-          className="bg-violet-600 px-4 py-2 rounded-sm text-violet-50 w-full"
+          className="bg-violet-600 px-4 py-2 rounded-sm text-violet-50 w-full font-medium"
         >
           LOG IN
         </button>
