7d7eb4f1b7029719068f31b2fca25e04494e1015
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-05-01 18:50:53 +0400

    fetching some data with react query

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,34 @@
 import { useAtom } from "jotai";
 import { authAtom } from "../Auth";
-import { useState } from 'react'
+import { useState } from "react";
+import { useQuery } from "react-query";
 
 const Home: React.FC = () => {
-  const [data, setData] = useState([])
   const [auth] = useAtom(authAtom);
-  const getData = async () => {
+  const { data } = useQuery("artists", async () => {
     const res = await fetch("http://localhost:8096/Artists", {
       headers: {
         Authorization: auth,
       },
     });
 
-    const data = await res.json();
-    console.log(data.Items)
-    setData(data.Items)
-  };
+    const artists = await res.json();
+    console.log(artists)
+    return artists.Items;
+  });
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-neutral-950 p-8">
-      <button
-        onClick={() => getData()}
-        className="text-violet-50 bg-violet-500 rounded-md px-4 py-2"
-      >
+      <button className="text-violet-50 bg-violet-500 rounded-md px-4 py-2">
         Get Data
       </button>
-      <pre className="max-w-4xl whitespace-normal text-violet-50 break-all">{data.map((item) => <div>{item.Name}</div>)}</pre>
+      {data && (
+        <pre className="max-w-4xl whitespace-normal text-violet-50 break-all">
+          {data.map((item: any) => (
+            <div key={item.Id}>{item.Name}</div>
+          ))}
+        </pre>
+      )}
     </div>
   );
 };
