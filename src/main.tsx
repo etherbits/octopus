760b13bcb1d79e54efa17ef66d8aa858a81f1e73
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-05-01 14:47:32 +0400

    adding react query

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./pages";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
