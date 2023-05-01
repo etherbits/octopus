a0a1a6fa7e27e684a205bc8a8b086c9de11736a0
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-05-01 12:53:18 +0400

    initial

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-neutral-950">
+      <h1 className="text-indigo-500 text-3xl">Octopus Player</h1>
+    </div>
+  );
+};
+
+export default App;
