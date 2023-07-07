7194d1101890699272548c46ddf7b73004ff54e5
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-07-07 20:32:33 +0400

    starting sidebar redesign

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -5,41 +5,6 @@ import TabGroup from "../TabGroup";
 const Sidebar = () => {
   return (
     <div className="flex flex-col gap-4 w-72 text-neutral-200 bg-neutral-950 p-4">
-      <div className="flex items-center gap-2">
-        <img
-          src="/assets/icons/octopus.svg"
-          className="w-[18px] h-[18px]"
-          alt="logo"
-        />
-        <h2 className="text-lg text-neutral-50">Octopus</h2>
-      </div>
-      <Search />
-      <TabGroup label="views">
-        <TabLink label="Home" to="/" iconPath="/assets/icons/home.svg" />
-        <TabLink
-          label="Albums"
-          to="/albums"
-          iconPath="/assets/icons/disc.svg"
-        />
-        <TabLink label="Songs" to="/songs" iconPath="/assets/icons/music.svg" />
-        <TabLink
-          label="Artists"
-          to="/artists"
-          iconPath="/assets/icons/users.svg"
-        />
-      </TabGroup>
-      <TabGroup label="playlists">
-        <TabLink
-          label="Playlist #1"
-          to="/playlist-1"
-          iconPath="/assets/icons/list.svg"
-        />
-        <TabLink
-          label="Playlist #2"
-          to="/playlist-1"
-          iconPath="/assets/icons/list.svg"
-        />
-      </TabGroup>
     </div>
   );
 };
