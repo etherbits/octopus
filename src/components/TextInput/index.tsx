d1bed65b546d0af980ea7be440c56507150e5b18
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-07-11 23:23:35 +0400

    fixing minor bug in text input

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -9,7 +9,7 @@ const TextInput: React.FC<Props> = ({ icon, placeholder }) => {
   const LeftIcon = Icon[icon || "AlertCircle"];
 
   return (
-    <div className="flex items-center rounded-md outline outline-2 outline-neutral-900 px-3 py-2 gap-2 group focus-within:outline-neutral-600">
+    <div className="flex items-center rounded-md outline outline-2 text-neutral-200 outline-neutral-900 px-3 py-2 gap-2 group focus-within:outline-neutral-600">
       <LeftIcon
         size={18}
         strokeWidth={1.5}
@@ -18,7 +18,7 @@ const TextInput: React.FC<Props> = ({ icon, placeholder }) => {
       <input
         type="text"
         placeholder={placeholder}
-        className="bg-transparent outline-none placeholder:text-neutral-400 text-sm group-focus-within:text-neutral-200"
+        className="bg-transparent outline-none placeholder:text-neutral-400 text-sm"
       />
     </div>
   );
