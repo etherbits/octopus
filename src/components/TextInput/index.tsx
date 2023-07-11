29e872b59af094c4b36e3ee2a89da40a5573ddae
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-07-11 23:08:45 +0400

    fixing v-centering in textinput

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -9,7 +9,7 @@ const TextInput: React.FC<Props> = ({ icon, placeholder }) => {
   const LeftIcon = Icon[icon || "AlertCircle"];
 
   return (
-    <div className="flex rounded-md outline outline-2 outline-neutral-900 text-neutral-200 px-3 py-2 gap-2">
+    <div className="flex items-center rounded-md outline outline-2 outline-neutral-900 text-neutral-200 px-3 py-2 gap-2">
       <LeftIcon size={18} color="#a3a3a3" strokeWidth={1.5} />
       <input
         type="text"
