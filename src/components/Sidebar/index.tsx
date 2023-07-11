c41b9291e6c641b27c3d77fdb5789cb14e49b6b0
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-07-11 21:06:20 +0400

    adding space at the end of the album cards

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -23,12 +23,12 @@ const Sidebar = () => {
     return albumData.Items;
   });
   return (
-    <div className="flex flex-col gap-5 p-4 pb-0 w-72 bg-neutral-950">
+    <div className="flex flex-col gap-5 p-5 pb-0 w-72 bg-neutral-950">
       <h2 className="text-neutral-200">Octopus</h2>
-      <ul className="flex flex-col gap-5 w-full text-neutral-200 overflow-y-scroll">
+      <ul className="flex flex-col gap-5 w-full text-neutral-200 overflow-y-auto ">
         {albums &&
           albums.map((album: Album) => (
-            <li key={album.Id}>
+            <li key={album.Id} className="last:mb-5">
               <AlbumCard album={album} />
             </li>
           ))}
