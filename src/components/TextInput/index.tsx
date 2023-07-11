d427a5077ab73d58a38aaa1669c03bff76426395
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-07-11 23:20:32 +0400

    adding text input focus color change

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -9,12 +9,16 @@ const TextInput: React.FC<Props> = ({ icon, placeholder }) => {
   const LeftIcon = Icon[icon || "AlertCircle"];
 
   return (
-    <div className="flex items-center rounded-md outline outline-2 outline-neutral-900 text-neutral-200 px-3 py-2 gap-2">
-      <LeftIcon size={18} color="#a3a3a3" strokeWidth={1.5} />
+    <div className="flex items-center rounded-md outline outline-2 outline-neutral-900 px-3 py-2 gap-2 group focus-within:outline-neutral-600">
+      <LeftIcon
+        size={18}
+        strokeWidth={1.5}
+        className="stroke-neutral-400 group-focus-within:stroke-neutral-200"
+      />
       <input
         type="text"
         placeholder={placeholder}
-        className="bg-transparent outline-none placeholder:text-neutral-400 text-sm"
+        className="bg-transparent outline-none placeholder:text-neutral-400 text-sm group-focus-within:text-neutral-200"
       />
     </div>
   );
